1c53e3f1042cbb3353a9428124d281ed761157d3
phasewalk1 <phasewalk1@proton.me>
2024-01-26 17:59:08 -0800

    tidy builtins.rs

--- src/builtins.rs
+++ src/builtins.rs
@@ -1,6 +1,27 @@
+use crate::primitive::{Atom, Value};
 use std::{collections::HashMap, rc::Rc};
 
-use crate::primitive::{Atom, Value};
+impl Default for crate::namespace::Environment {
+    fn default() -> Self {
+        let mut operators = HashMap::new();
+        let mut keywords = HashMap::new();
+
+        crate::load_builtin_ops!(operators,
+            b'+' => crate::builtins::add,
+            b'-' => crate::builtins::sub,
+            b'*' => crate::builtins::mul,
+            b'/' => crate::builtins::div,
+            b'=' => crate::builtins::eq,
+            b'>' => crate::builtins::ge,
+        );
+        crate::load_builtin_keywords!(keywords, String::from("if") => ());
+
+        Self {
+            operators,
+            keywords,
+        }
+    }
+}
 
 pub(crate) fn add(a: Atom, b: Atom) -> Value {
     Value::Atom(Atom(a.0 + b.0))
@@ -31,24 +52,5 @@ pub(crate) fn ge(a: Atom, b: Atom) -> Value {
         Value::T
     } else {
         Value::Nil
-    }
-}
-impl Default for crate::namespace::Environment {
-    fn default() -> Self {
-        let mut operators = HashMap::new();
-        let mut keywords = HashMap::new();
-        crate::load_builtin_ops!(operators,
-            b'+' => crate::builtins::add,
-            b'-' => crate::builtins::sub,
-            b'*' => crate::builtins::mul,
-            b'/' => crate::builtins::div,
-            b'=' => crate::builtins::eq,
-            b'>' => crate::builtins::ge,
-        );
-        crate::load_builtin_keywords!(keywords, String::from("if") => ());
-        Self {
-            operators,
-            keywords,
-        }
     }
 }
